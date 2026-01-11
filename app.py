63e3ec66c92686d5c6f695855668beec18ba7797
aka247 <a_k@gmx.at>
2026-01-11 01:09:17 +0100

    fix errors

--- app.py
+++ app.py
@@ -173,9 +173,10 @@ def index():
     return render_template("index.html", projects=projects, artworks=artworks)
  
 # Freeze the app, to deploy as static site
-#freezer = Freezer(app)
-#app.config['FREEZER_RELATIVE_URLS'] = True
+freezer = Freezer(app)
+app.config['FREEZER_RELATIVE_URLS'] = True
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5017)
- #   freezer.freeze()
+   #app.run(debug=True, port=5017)
+    freezer.freeze()
